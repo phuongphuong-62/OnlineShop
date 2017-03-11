9683d5b5e8fd995423baf3247fb8b99302ea5db8
phuongphuong-62 <huynhthitrucphuong69@gmail.com>
2017-03-11 18:29:40 +0700

    OnlShop

---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,54 @@
+﻿using Model.Dao;
+using OnlineShop.Areas.Admin.Models;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)//form rỗng cách duy nhất để biết liệu có bất kỳ lỗi xác nhận
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.UserName, Encryptor.MD5Hash(model.Password));
+                if (result == 1)
+                {
+                    var user = dao.GetById(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserID = user.ID;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else if (result == 0)
+                {
+                    ModelState.AddModelError("", "TÀI KHOẢN CỦA BẠN KHÔNG TỒN TẠI .");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "TÀI KHOẢN CỦA BẠN BỊ KHÓA.");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "MẬT KHẨU CỦA BẠN KHÔNG ĐÚNG.");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "TÀI KHOẢN CỦA BẠN KHÔNG ĐÚNG.");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
---
+++ OnlineShop/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Model.Dao;
+using Model.EF;
+using OnlineShop.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class UserController : BaseController
+    {
+        // GET: Admin/User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var encryptedMd5Pas = Encryptor.MD5Hash(user.Password);
+                user.Password = encryptedMd5Pas;
+                long id = dao.Insert(user);
+                if (id > 0)
+                {
+                    return RedirectToAction("Index", "User");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thêm user thành công");
+                }
+            }
+            return View("Index");
+        }
+    }
+}
